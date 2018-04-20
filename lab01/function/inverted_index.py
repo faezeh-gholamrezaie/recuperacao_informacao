cb18445c38c09ebd130db6e8e3d2fc10d00e306f
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2018-04-20 19:36:42 -0300

    Corrigindo bugs no lab

--- lab01/function/inverted_index.py
+++ lab01/function/inverted_index.py
@@ -1,14 +1,51 @@
 #coding: utf-8
+import nltk as n
+
+inverted_index = {}
 def build(data):
-    inverted_index = {}
-    for row in data:
-        doc = row[1].split()
-        doc_id = row[2]
-        list_doc_id = []
-        for word in doc:
-            # print "Palavra %s - DocID %s" % (word, doc_id)
-            list_doc_id.append(doc_id)
-            # print list_doc_id
-            inverted_index[word] = list_doc_id
-        list_doc_id = []
+    data["noticia"] = data.titulo + " " + data.conteudo
+    for index, doc in data.iterrows():
+        doc.noticia = doc.noticia.lower()
+        words = [str(word) for word in n.word_tokenize(doc.noticia)]
+        for word in words:
+            # print type(inverted_index.setdefault(word, []))
+            inverted_index.setdefault(word, []).append(doc.idNoticia)
+
+
+    for word in inverted_index.keys():
+        inverted_index[word] = set(inverted_index[word])
+
     return inverted_index
+
+def search(term):
+    term = n.word_tokenize(term)
+
+    if len(term) == 1:
+        return _search_one_term(term[0])
+    elif len(term) == 3 and term[1].upper() == "AND":
+        return _search_and(term[0], term[2])
+    elif len(term) == 3 and term[1].upper() == "OR":
+        return _search_or(term[0], term[2])
+
+
+def _search_one_term(term):
+    if inverted_index.has_key(term.lower()):
+        return sorted(list(inverted_index[term.lower()]))
+    else:
+        return None
+
+def _search_and(first_term, second_term):
+    if inverted_index.has_key(first_term.lower()) and inverted_index.has_key(second_term.lower()):
+        return sorted(list(inverted_index[first_term.lower()] & inverted_index[second_term.lower()]))
+    else:
+        return None
+
+def _search_or(first_term, second_term):
+    if inverted_index.has_key(first_term.lower()) and inverted_index.has_key(second_term.lower()):
+        return sorted(list(inverted_index[first_term.lower()] | inverted_index[second_term.lower()]))
+    elif inverted_index.has_key(first_term.lower()):
+        return sorted(list(inverted_index[first_term.lower()]))
+    elif inverted_index.has_key(second_term.lower()):
+        return sorted(list(inverted_index[second_term.lower()]))
+    else:
+        return None
--- lab01/setup.py
+++ lab01/setup.py
@@ -1,6 +1,37 @@
-import function.reader_csv as csv
+#coding: utf-8
 import function.inverted_index as inverted_index
+import pandas as p
 
-data = csv.get_data('data-set/noticias_estadao.csv')
-dic = inverted_index.build(data)
-print dic
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+data = p.read_csv("data-set/noticias_estadao.csv", encoding = "utf-8")
+
+inverted_index.build(data)
+
+# Sanity Check
+
+# Campina AND Grande
+assert len(inverted_index.search("Campina AND Grande")) == 12
+assert len(inverted_index.search("Campina OR Grande")) == 1656
+assert inverted_index.search("Campina AND Grande") == [1068, 1370, 1770, 1952, 1987, 2763, 2777, 2779, 4802, 5382, 5870, 6694]
+assert len(inverted_index.search("Campina OR Grande")) == len(inverted_index.search("Campina")) + len(inverted_index.search("Grande")) - len(inverted_index.search("Campina AND Grande"))
+
+# debate, presidenciável (AND e OR)
+
+assert len(inverted_index.search("debate OR presidencial")) == 1770
+
+assert len(inverted_index.search("debate AND presidencial")) == 201
+
+# presidenciáveis, corruptos (AND e OR)
+
+assert len(inverted_index.search("presidenciáveis OR corruptos")) == 164
+
+assert len(inverted_index.search("presidenciáveis AND corruptos"))== 0
+
+# Belo, Horizonte (AND e OR)
+
+assert len(inverted_index.search("Belo OR Horizonte")) == 331
+
+assert len(inverted_index.search("Belo AND Horizonte")) == 242
